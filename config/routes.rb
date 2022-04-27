3863d2e0bd734e0221c7e89ac2ff986962dc9c51
Daniel Armbruster <danielgundam00@gmail.com>
2022-04-27 11:34:16 -0600

    commit tree 70254e444206e67aa0f3c36efc2c86079ab4e28c

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items, only: [:index ]
+  
+  resources :users, only: [:show]  do
+    # nested resource for items
+    resources :items, only: [:show, :index, :create]
+  end
 end
